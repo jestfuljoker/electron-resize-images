60106f7fdc41100964e1c05e8740390f626b5aee
Christofer Assis <chris.f.assis18@gmail.com>
2024-01-20 14:08:35 -0300

    chore: add typing to window

--- electron/electron-env.d.ts
+++ electron/electron-env.d.ts
@@ -1,27 +1,15 @@
 /// <reference types="vite-plugin-electron/electron-env" />
 
 declare namespace NodeJS {
-  interface ProcessEnv {
-    /**
-     * The built directory structure
-     *
-     * ```tree
-     * ├─┬─┬ dist
-     * │ │ └── index.html
-     * │ │
-     * │ ├─┬ dist-electron
-     * │ │ ├── main.js
-     * │ │ └── preload.js
-     * │
-     * ```
-     */
-    DIST: string
-    /** /dist/ or /public/ */
-    VITE_PUBLIC: string
-  }
+	interface ProcessEnv {
+		DIST: string;
+		VITE_PUBLIC: string;
+	}
 }
 
 // Used in Renderer process, expose in `preload.ts`
 interface Window {
-  ipcRenderer: import('electron').IpcRenderer
+	ipcRenderer: import('electron').IpcRenderer;
+	os: typeof import('os');
+	path: typeof import('path');
 }
