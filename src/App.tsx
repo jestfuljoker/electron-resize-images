7aa618f72119b822bd51f6a675b3f7fde069ae2c
Christofer Assis <chris.f.assis18@gmail.com>
2024-01-20 14:15:09 -0300

    feat: Implement components and renderer communication process

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,38 @@
+import { useEffect, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+import { Container, Form, Header } from './components';
+
 export function App() {
-	return <h1 className="text-3xl font-bold underline text-red-600">Hello World!!!!!</h1>;
+	const [files, setFiles] = useState<File[]>([]);
+
+	function handleFilesChange(files: File[]) {
+		setFiles(files);
+	}
+
+	useEffect(() => {
+		window.ipcRenderer.on('image:done', (_, isLast, error, fileName) => {
+			if (isLast) {
+				setFiles([]);
+				toast.success(`Arquivo(s) redimensionado(s) com sucesso!`);
+			}
+
+			if (error) {
+				toast.error(`Ops, aconteceu um erro ao redimensionar o arquivo ${fileName}`);
+			}
+		});
+	}, []);
+
+	return (
+		<>
+			<div>
+				<Toaster />
+			</div>
+
+			<Container>
+				<Header onFilesChange={handleFilesChange} />
+				{files.length > 0 && <Form files={files} />}
+			</Container>
+		</>
+	);
 }
